6ad92e3ebedba492257f9f906c8ce64e09176263
Dmytro Savchuk <dmytro.savchuk.ua@gmail.com>
2024-02-19 15:45:59 +0200

    weather lesson completed

--- learningxamarin/learningxamarin/MainPage.xaml.cs
+++ learningxamarin/learningxamarin/MainPage.xaml.cs
@@ -5,58 +5,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
 
 namespace learningxamarin
 {
     public partial class MainPage : ContentPage
     {
-
-        Button button;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-
-     /*   protected override void OnAppearing()
+        private async void getWeather_Clicked(object sender, EventArgs e)
         {
-            StackLayout stack = new StackLayout();
-
-            Label label = new Label();
-            label.Text = "hello world c#";
-            label.TextTransform = TextTransform.Uppercase;
-            label.FontSize = 25;
-
-            button = new Button();
-            button.Text = "Click me! C#";
-            button.BackgroundColor = Color.Black;
-            button.TextColor = Color.White;
-            button.Clicked += Button_Clicked;
-
-            stack.Children.Add(label);
-            stack.Children.Add(button);
-
-            Content = stack;
-        }*/
-
-        private async void Button_Clicked(object sender, EventArgs e)
-        {
-            if (string.IsNullOrEmpty(nameField.Text))
-                errorField.Text = "No name specified";
-            else if (string.IsNullOrEmpty(emailField.Text))
-                errorField.Text = "No email specified";
-            else if (string.IsNullOrEmpty(passField.Text))
-                errorField.Text = "No password specified";
-            else if (!checkField.IsChecked)
-                errorField.Text = "Check box incorrect";
-            else
+            const string apiKey = "c77db93519429fc7bad7fdb7feb1c182";
+            string city = userInput.Text.Trim();
+            if (city.Length < 2) 
             {
-                errorField.Text = "";
-                buttonSend.Text = "OK";
-                buttonSend.TextColor = Color.Green;
-                await DisplayAlert("form data", "all data gainend", "OK");
+               await DisplayAlert("Error", "City name is to short", "ok");
+               return;
             }
+            string apiPath = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric";
+            HttpClient client = new HttpClient();
+            string response = await client.GetStringAsync(apiPath);
+
+            var json = JObject.Parse(response);
+            double temp = double.Parse(json["main"]["temp"].ToString()) ;
+            result.Text = "Current temperature is " + Math.Floor(temp)+"°";
 
         }
     }
